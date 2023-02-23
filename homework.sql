d334bf17c2d53a1263fe0563992505d5987ef94a
newsnick <44749115+newsnick@users.noreply.github.com>
2023-02-23 07:46:57 +0000

    homework done

--- homework.sql
+++ homework.sql
@@ -19,3 +19,75 @@
  10. Our school changes grade system to 0..10 range. Safely migrate old data and change constraints according to the new system rules.
  11. Expel students whose grades are worse that 4.0
  */
+
+ -- @block
+CREATE DATABASE IF NOT EXISTS college;
+
+-- @block
+USE college;
+
+-- @block
+DROP TABLE IF EXISTS students;
+
+-- @block
+CREATE TABLE students (
+  name VARCHAR(64) NOT NULL,
+  lastname VARCHAR(64) NOT NULL,
+  avg_mark DECIMAL(3,1) UNSIGNED,
+  sex VARCHAR(64) CHECK (sex = 'M' OR sex = 'F')
+);
+
+-- @block
+INSERT INTO students (name, lastname, avg_mark, sex)
+VALUES 
+  ('Simon', 'Lowe', 2.3, 'M'),
+  ('Jimmy', 'Lee', 4.6, 'M'),
+  ('Sandra', 'Brown', 1.2, 'F'),
+  ('Steven', 'Kraft', 3.4, 'M'),
+  ('Karen', 'Hill', 2.8, 'F');
+
+-- @block
+ALTER TABLE students 
+ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY FIRST;
+
+-- @block
+ALTER TABLE students 
+MODIFY COLUMN sex CHAR(1);
+
+-- @block
+ALTER TABLE students 
+CHANGE COLUMN name first_name VARCHAR(64) NOT NULL;
+
+-- @block
+SELECT * FROM students 
+WHERE avg_mark > 4;
+
+-- @block
+SELECT * FROM students 
+WHERE avg_mark < 3 OR avg_mark > 4;
+
+-- @block
+SELECT * FROM students 
+WHERE first_name LIKE 'A%';
+
+-- @block
+SELECT * FROM students 
+WHERE avg_mark IN (2.2, 3.1, 4.7);
+
+-- @block
+CREATE VIEW male_students AS
+SELECT * FROM students WHERE sex = 'M';
+
+-- @block
+CREATE VIEW female_students AS
+SELECT * FROM students WHERE sex = 'F';
+
+-- @block
+CREATE VIEW male_students AS
+SELECT * FROM students WHERE sex = 'M';
+
+-- @block
+SELECT DISTINCT avg_mark FROM students;
+
+-- @block
+DELETE FROM students WHERE avg_mark < 4;
