221bcb3b8bb8bf3e7bf418c7565909e4262a4466
Mark Chigrin <mark@ilmpc.xyz>
2023-03-03 18:47:54 +0400

    Add homework

--- classwork.sql
+++ classwork.sql
@@ -10,7 +10,17 @@ FROM Students;
 SELECT *
 FROM Courses;
 
+-- @block
+SELECT * 
+FROM StudentsToCourses;
 
 -- @block
 DELETE FROM Students
 WHERE id = 1;
+
+-- @block
+DELETE FROM Courses
+WHERE id = 2;
+
+-- @block
+UPDATE Students SET id = 33 WHERE id = 5;
---
+++ homework.sql
@@ -0,0 +1,40 @@
+USE hr;
+
+
+-- @block
+-- Find employees names that has max salary in their department
+SELECT first_name,
+  last_name
+FROM employees;
+
+
+-- @block
+SELECT department_id,
+  MAX(salary) AS salary
+FROM employees
+GROUP BY department_id
+ORDER BY salary DESC;
+
+
+-- @block
+SELECT first_name,
+  last_name,
+  department_id
+FROM employees
+WHERE salary IN (
+    SELECT MAX(salary)
+    FROM employees
+    GROUP BY department_id
+  );
+
+
+-- @block
+SELECT first_name,
+  last_name, e.department_id, e.salary
+FROM employees as e
+  INNER JOIN (
+    SELECT department_id,
+      MAX(salary) AS salary
+    FROM employees
+    GROUP BY department_id
+  ) as s ON e.department_id = s.department_id AND e.salary = s.salary;
