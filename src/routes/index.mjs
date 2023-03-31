64e9951b8e80335cfc92ce5413bf8785f2dff530
Mark Chigrin <mark@ilmpc.xyz>
2023-03-31 16:31:09 +0400

    Add new handler

--- src/routes/index.mjs
+++ src/routes/index.mjs
@@ -6,15 +6,33 @@ async function getUsers() {
   return users.find().toArray()
 }
 
+async function createUser(req) {
+  const users = this.mongo.db.collection('users')
+  const data = req.body
+
+  const { insertedId } = await users.insertOne({
+    phone_number: data.phone,
+    fullname: data.fullName,
+  })
+  return { userId: insertedId }
+}
+
 export default async function routes(fastify, options) {
   fastify.get('/ping', function (req, reply) {
     reply.send({ message: 'pong' })
   })
 
   fastify.route({
-    method: 'POST',
-    url: '/',
+    method: 'GET',
+    url: '/users',
     handler: getUsers,
+    schema: {},
+  })
+
+  fastify.route({
+    method: 'POST',
+    url: '/users',
+    handler: createUser,
     schema: {
       body: {},
     },
